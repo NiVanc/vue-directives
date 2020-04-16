98da9e380e2a9e297b243dccef57a67b22080ee8
NiVanc <nico@izebra.be>
2020-04-16 21:21:52 +0200

    Create a simple custom directive

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,14 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Directives</h1>
+        <h1>Built-in Directives</h1>
+        <p v-text="'Some text'"></p>
+      </div>
+    </div>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <p v-highlight>A simple custom directive</p>
       </div>
     </div>
   </div>
